80f3cde0cc4e48e41b4a2cb5c6154e5a719200eb
Artem Barafanov <st117258@student.spbu.ru>
2024-02-19 09:59:17 +0300

    [refactoring_and_print] Add the first example of refactoring programm

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 @echo off
-
+setlocal EnableDelayedExpansion
+::
 :: Вид сборки / суффикс в названии каталога сборки
 ::
 :: "Ninja" / "ninja"
@@ -20,5 +21,14 @@ cd %BUILD_FOLDER%
 cmake -G %BUILD_TYPE% ..\%SOURCE_FOLDER%
 cmake --build .
 
-copy ..\%SOURCE_FOLDER%\bubble_sort\run_bubble_sort.bat .\bubble_sort
-copy ..\%SOURCE_FOLDER%\bubble_sort_mf\run_bubble_sort_mf.bat .\bubble_sort_mf
+set arr[0].file=run_bubble_sort.bat
+set arr[1].file=run_bubble_sort_mf.bat
+set arr[2].file=run_refactoring_and_print.bat
+
+set arr[0].folder=bubble_sort
+set arr[1].folder=bubble_sort_mf
+set arr[2].folder=refactoring_and_print
+
+for /L %%i in (0,1,2) do ( 
+	copy ..\%SOURCE_FOLDER%\!arr[%%i].folder!\!arr[%%i].file! .\!arr[%%i].folder!
+)
